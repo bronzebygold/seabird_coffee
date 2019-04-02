003845ec6d37a4f171798f486375e8b9429b662e
Daniel Miller <lontano.composer@gmail.com>
2019-04-02 18:52:51 -0400

    regex expressions for animations of merch grid

--- assets/js/merchandise.js
+++ assets/js/merchandise.js
@@ -1,56 +1,71 @@
 window.onload = function(){
     const a = [
 
-        `<div id=\"m1\">
+        `<div id=\"m1\" onclick=\"deleteNode(this)\">
             <div>
-                <img src=\"assets/media/photos/m1.png\" alt=\"Seabird Crewneck Sweatshirt\">
+                <div>
+                    <img src=\"assets/media/photos/m1.png\" alt=\"Seabird Crewneck Sweatshirt\">
+                </div>
+                <div>Seabird Crewneck Sweatshirt<br>$50</div>
             </div>
-            <div>Seabird Crewneck Sweatshirt<br>$50</div>
         </div>`,
 
-        `<div id=\"m2\">
+        `<div id=\"m2\" onclick=\"deleteNode(this)\">
             <div>
-                <img src=\"assets/media/photos/m2.png\" alt=\"Seabird Hoodie\">
+                <div>
+                    <img src=\"assets/media/photos/m2.png\" alt=\"Seabird Hoodie\">
+                </div>
+                <div>Seabird Hoodie<br>$40</div>
             </div>
-            <div>Seabird Hoodie<br>$40</div>
         </div>`,
 
-        `<div id=\"m3\">
+        `<div id=\"m3\" onclick=\"deleteNode(this)\">
             <div>
-                <img src=\"assets/media/photos/m3.png\" alt=\"Seabird Adult T-Shirt\">
+                <div>
+                    <img src=\"assets/media/photos/m3.png\" alt=\"Seabird Adult T-Shirt\">
+                </div>
+                <div>Seabird Adult T-Shirt<br>$20</div>
             </div>
-            <div>Seabird Adult T-Shirt<br>$20</div>
         </div>`,
 
-        `<div id=\"m4\">
+        `<div id=\"m4\" onclick=\"deleteNode(this)\">
             <div>
-                <img src=\"assets/media/photos/m4.png\" alt=\"Seabird Toddler T-Shirt\">
+                <div>
+                    <img src=\"assets/media/photos/m4.png\" alt=\"Seabird Toddler T-Shirt\">
+                </div>
+                <div>Seabird Toddler T-Shirt<br>$18</div>
             </div>
-            <div>Seabird Toddler T-Shirt<br>$18</div>
         </div>`,
 
-        `<div id=\"m5\">
+        `<div id=\"m5\" onclick=\"deleteNode(this)\">
             <div>
-                <img src=\"assets/media/photos/m5.png\" alt=\"Seabird Youth T-Shirt\">
+                <div>
+                    <img src=\"assets/media/photos/m5.png\" alt=\"Seabird Youth T-Shirt\">
+                </div>
+                <div>Seabird Youth T-Shirt<br>$18</div>
             </div>
-            <div>Seabird Youth T-Shirt<br>$18</div>
         </div>`,
 
-        `<div id=\"m6\">
+        `<div id=\"m6\" onclick=\"deleteNode(this)\">
             <div>
-                <img src=\"assets/media/photos/m6.png\" alt=\"Mexican Blanket\">
+                <div>
+                    <img src=\"assets/media/photos/m6.png\" alt=\"Mexican Blanket\">
+                </div>
+                <div>Mexican Blanket<br>$20</div>
             </div>
-            <div>Mexican Blanket<br>$20</div>
         </div>`,
 
-        `<div id=\"m7\">
+        `<div id=\"m7\" onclick=\"deleteNode(this)\">
             <div>
-                <img src=\"assets/media/photos/m7.png\" alt=\"Seabird Gift Card\">
+                <div>
+                    <img src=\"assets/media/photos/m7.png\" alt=\"Seabird Gift Card\">
+                </div>
+                <div>Seabird Gift Card<br>$20</div>
             </div>
-            <div>Seabird Gift Card<br>$20</div>
         </div>`
 
         ];
 
-    document.getElementById("mcontent").innerHTML = a.join("");
+    document.querySelector(".mcontent").innerHTML = a.join("");
 }
+
